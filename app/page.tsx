9d12210ba9495ba30668fa801199d30c456e21af
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-12-15 18:39:32 -0600

    Marquee added

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-import Imagen3 from '@/app/images/imagen3.png'
+import Imagen3 from './images/imagen3.png'
+import Image from 'next/image'
 import Marquee from "react-fast-marquee"
-import Image from "next/image"
 
 export default function Home() {
   return (
     <>
-  <Marquee>
-  
-<h1>nelll</h1>
-  </Marquee>
-  
-  
+      <Marquee>
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+        <Image src={Imagen3} alt='nodog' width={300} height={30} />
+      </Marquee>
+
     </>
   )
 }
