37d03645eafbc74f16d96d84f7d74cb4a9068aff
ArkalKeirosRas <recargamarcas6@gmail.com>
2023-12-27 00:06:48 -0600

    gifs added

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,19 +1,16 @@
 'use client'
 import {rubik} from '@/app/ui/fonts'
 import Image from "next/image"
-import Papeleria from '@/app/images/papeleria.png'
-import RobotLearning from '@/app/images/robotlearning.jpeg'
+import GifLeagueOfLegends from '@/public/gif_navbar/LeagueOfLegends.gif'
 import Link from 'next/link'
 export default function Navbar() {
 	return(
 	<>
 		<nav className="w-11/12 mx-auto my-auto mt-4 h-16 bg-black  text-white flex items-center  justify-center   ">
-		<Image onContextMenu={(e) => e.preventDefault()}  src={Papeleria} alt="price shoes logo" width={70} height={60} className="mr-5 w-12  h-14 rounded pointer-events-none md:mr-14" />
-			<Link href='/' onContextMenu={(e) => e.preventDefault()}  className={`flex w-auto h-full items-center text-4xl md:text-6xl lg:text-7xl text-slate-200 justify-center ${rubik.className} antialiased`}> Ciber Arkal</Link>
-			<div className="flex w-auto h-full items-center justify-center">
-				<Image onContextMenu={(e) => e.preventDefault()} src={RobotLearning} alt="price shoes logo" width={70} height={60} className="ml-5 w-11 rounded pointer-events-none md:ml-14" />
-			</div>
+		<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={70} height={60} className="rounded w-28  h-28  pointer-events-none " />
+			<Link href='/' onContextMenu={(e) => e.preventDefault()}  className={`flex w-auto h-full items-center text-5xl md:text-6xl lg:text-7xl text-slate-400 justify-center ${rubik.className} antialiased`}>ARKAL</Link>
+			<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={70} height={60} className="rounded w-28  h-28  pointer-events-none " />
 		</nav>
-		<div className='w-11/12 border-b-2 border-lime-300  mx-auto my-auto mt-1 bg-lime-300 md:w-8/12 '></div>
+		<div className='w-11/12 border-b-2 border-amber-400 absolute right-0 left-0  mx-auto my-auto mt-1  md:w-8/12 '></div>
 	</>)
 }
