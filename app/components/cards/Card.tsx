5f5193dccce24dd0c5fa5cbeec3dbc9f7d34d6da
ArkalKeirosRas <recargamarcas6@gmail.com>
2024-08-24 13:22:27 -0600

    eagle navbar  added

--- app/components/cards/Card.tsx
+++ app/components/cards/Card.tsx
@@ -37,7 +37,9 @@ export default function Card() {
   ];
 
   return (
-    <div className=" flex flex-wrap w-full mt-8  items-center justify-center mx-auto my-auto   md:mt-14 md:w-11/12">
+  <div className="w-full flex flex-col items-center justify-center">
+    <h1 className={`text-white p-2 w-11/12  mx-auto my-auto md:text-center`}>En <b>Arkal</b> puedes seleccionar <b>ropa</b> y <b>calzado</b> dando click en el icono de <b>priceshoes</b></h1>
+    <div className=" flex flex-wrap w-full mt-1  items-center justify-center mx-auto my-auto   md:mt-14 md:w-11/12">
       {cardImages.map((image) => {
         return (
           <Link
@@ -45,11 +47,13 @@ export default function Card() {
             href={image.href}
             className=" transform hover:scale-105 transition duration-100 ease-in-out "
           >
-            <Image  src={image.src} alt={image.alt} className=" w-[200px] h-[300px] mr-[5px] rounded mt-[5px] pointer-events-none " />
+            <Image  src={image.src} alt={image.alt} className=" w-[170px] h-[270px] mr-[12px] rounded mb-[15px] mt-[5px] pointer-events-none  " />
             {/* <p className=" w-full absolute top-0  text-white flex items-center justify-center text-xl font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">{image.title}</p> */}
           </Link>
         );
       })}
     </div>
+
+  </div>  
   );
 }
