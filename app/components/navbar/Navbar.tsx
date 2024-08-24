48cb73451d83ea386076ca44ebe23ade9fff0675
ArkalKeirosRas <recargamarcas6@gmail.com>
2024-08-24 13:29:08 -0600

    sizes eagle image navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,9 +7,9 @@ export default function Navbar() {
 	return(
 	<>
 		<nav className="w-11/12 mx-auto my-auto mt-2 h-16 bg-black  text-white flex items-center  justify-center md:w-9/12 justify-evenly   ">
-		<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={70} height={60} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30 " />
+		<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30 " />
 			<Link href='/' onContextMenu={(e) => e.preventDefault()}  className={`flex w-auto h-full items-center text-5xl md:text-6xl lg:text-7xl text-cyan-200 justify-center ${rubik.className} antialiased`}>Arkal</Link>
-			<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={150} height={140} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30" />
+			<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30" />
 		</nav>
 		<div className='w-11/12 border-b-2 border-sky-700 absolute right-0 left-0  mx-auto my-auto mt-3  md:w-8/12 '></div>
 	</>)
