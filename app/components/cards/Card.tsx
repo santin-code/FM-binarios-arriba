9ceb0ab3521794983979d5524a5e6b2d1cbddd45
ArkalKeirosRas <recargamarcas6@gmail.com>
2024-08-29 09:11:07 -0600

    edited cards

--- app/components/cards/Card.tsx
+++ app/components/cards/Card.tsx
@@ -1,59 +1,63 @@
-import Image from "next/image";
 import Link from "next/link";
 import Home1 from "./home1.jpg";
 import Papeleria from '@/public/imaCards/papeleria.png'
 import Perifericos from '@/public/imaCards/perifericos.png'
+import { londrina } from '@/app/ui/fonts'
+import { GiNotebook } from "react-icons/gi";
+
 
 export default function Card() {
-  const cardImages = [
+  const cardIcon = [
     {
       id: 1,
       href: "/priceshoes",
       src: Home1,
-      alt: "imagen d",
-      title:'Compra aquí'
+      nameIcon: `${<GiNotebook />}`,
+      title: 'Compra aquí'
     },
     {
       id: 2,
       href: '/perifericos',
       src: Papeleria,
-      alt: "imagen de price shoes",
-      title:'Perifericos'
+      nameIcon: "imagen de price shoes",
+      title: 'Perifericos'
     },
     {
       id: 3,
       href: "https://www.google.com",
       src: Perifericos,
-      alt: "imagen de price shoes",
-      title:'Papeleria'
+      nameIcon: "imagen de price shoes",
+      title: 'Papeleria'
     },
     {
       id: 4,
       href: "https://www.google.com",
       src: Perifericos,
-      alt: "imagen de price shoes",
-      title:'Para el hogar'
+      nameIcon: "imagen de price shoes",
+      title: 'Para el hogar'
     },
   ];
 
   return (
-  <div className="w-full flex flex-col items-center justify-center">
-    <h1 className={`text-white p-2 w-11/12  mx-auto my-auto md:text-center`}>En <b>Arkal</b> puedes seleccionar <b>ropa</b> y <b>calzado</b> dando click en el icono de <b>priceshoes</b></h1>
-    <div className=" flex flex-wrap w-full mt-1  items-center justify-center mx-auto my-auto   md:mt-14 md:w-11/12">
-      {cardImages.map((image) => {
-        return (
-          <Link
-            key={image.id}
-            href={image.href}
-            className=" transform hover:scale-105 transition duration-100 ease-in-out "
-          >
-            <Image  src={image.src} alt={image.alt} className=" w-[170px] h-[270px] mr-[12px] rounded mb-[15px] mt-[5px] pointer-events-none  " />
-            {/* <p className=" w-full absolute top-0  text-white flex items-center justify-center text-xl font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">{image.title}</p> */}
-          </Link>
-        );
-      })}
-    </div>
+    <div className="w-full flex flex-col items-center justify-center">
+      <div className=" flex flex-wrap w-full mt-1  items-center justify-center mx-auto my-auto   md:mt-14 md:w-11/12">
+        {cardIcon.map((icon) => {
+          return (
+            <Link
+              key={icon.id}
+              href={icon.href}
+              className=" transform hover:scale-105 transition duration-100 ease-in-out "
+            >
+              <div className="border border-white  w-[8rem] h-[8rem] mx-[4px] rounded ">
+              <div className="text-white">{icon.nameIcon}</div>
+                <div className={`text-white ${londrina.className}`}>{icon.title}</div>
+              </div>
 
-  </div>  
+            </Link>
+          );
+        })}
+      </div>
+
+    </div>
   );
 }
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -7,10 +7,10 @@ export default function Navbar() {
 	return(
 	<>
 		<nav className="w-11/12 mx-auto my-auto mt-2 h-16 bg-black  text-white flex items-center  justify-center md:w-9/12 justify-evenly   ">
-		<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30 " />
-			<Link href='/' onContextMenu={(e) => e.preventDefault()}  className={`flex w-auto h-full items-center text-5xl md:text-6xl lg:text-7xl text-cyan-200 justify-center ${rubik.className} antialiased`}>Arkal</Link>
-			<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-24 mt-2 rounded-lg  h-20  pointer-events-none md:w-30  md:h-30" />
+		<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-[65px] mt-2 rounded-lg  h-[72]  pointer-events-none md:w-30  md:h-30 " />
+			<Link href='/' onContextMenu={(e) => e.preventDefault()}  className={`flex w-auto h-full items-center text-5xl md:text-6xl lg:text-7xl text-teal-100 justify-center ${rubik.className} antialiased`}>ARKAL</Link>
+			<Image onContextMenu={(e) => e.preventDefault()}  src={GifLeagueOfLegends} alt="price shoes logo" width={80} height={70} className="rounded w-24 mt-2 rounded-lg w-[65px] h-[72]  pointer-events-none md:w-30  md:h-30" />
 		</nav>
-		<div className='w-11/12 border-b-2 border-sky-700 absolute right-0 left-0  mx-auto my-auto mt-3  md:w-8/12 '></div>
+		<div className='w-11/12 border-b-2 border-amber-100 absolute right-0 left-0  mx-auto my-auto mt-3  md:w-8/12 '></div>
 	</>)
 }
